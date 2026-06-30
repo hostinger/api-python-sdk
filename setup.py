972c72d3422e2a70d68b75c1ace37e2c4e2d4f15
Žygintas Rimkus <zygintas@hostinger.com>
2026-06-30 11:44:50 +0300

    Packages: update requirements

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     url="https://github.com/hostinger/api-python-sdk",
     keywords=["hostinger", "openapi", "python", "sdk", "rest", "api"],
     install_requires=REQUIRES,
+    python_requires=PYTHON_REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     license="MIT",

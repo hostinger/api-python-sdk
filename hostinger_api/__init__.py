d54c39ac71922ba3ac1ae7f4a4d78056c43116e2
zydrunasdranginishost <zydrunasdranginishost@users.noreply.github.com>
2026-08-14 11:19:35 +0000

    chore: sdk update

--- hostinger_api/__init__.py
+++ hostinger_api/__init__.py
@@ -12,7 +12,7 @@
 """  # noqa: E501
 
 
-__version__ = "3.29.7"
+__version__ = "3.29.8"
 
 # import apis into sdk package
 from hostinger_api.api.agency_hosting_cache_api import AgencyHostingCacheApi
--- hostinger_api/api_client.py
+++ hostinger_api/api_client.py
@@ -88,7 +88,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'hostinger_api-python/3.29.7'
+        self.user_agent = 'hostinger_api-python/3.29.8'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):

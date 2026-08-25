0b6574aaf30d7ba318eb6a047166876381213b8a
ugnsma <ugnsma@users.noreply.github.com>
2026-08-25 07:31:34 +0000

    feat: own downstream release workflows in the generator (#117)

--- hostinger_api/__init__.py
+++ hostinger_api/__init__.py
@@ -12,7 +12,7 @@
 """  # noqa: E501
 
 
-__version__ = "3.40.1"
+__version__ = "3.41.0"
 
 # import apis into sdk package
 from hostinger_api.api.agency_hosting_cache_api import AgencyHostingCacheApi
--- hostinger_api/configuration.py
+++ hostinger_api/configuration.py
@@ -508,7 +508,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.43.1\n"\
-               "SDK Package Version: 3.40.1".\
+               "SDK Package Version: 3.41.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
